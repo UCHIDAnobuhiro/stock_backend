e81ae8c0ee92943ab2996f636c4660036388bd58
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-09-23 15:38:28 +0900

    feat/DBから値を取得する機能を実装

--- internal/domain/repository/candle_repository.go
+++ internal/domain/repository/candle_repository.go
@@ -8,13 +8,7 @@ import (
 // CandleRepository　はロウソク足の永続化を抽象化します
 type CandleRepository interface {
 	// データベースを検索
-	// Find(ctx context.Context, symbol, interval string, outputsize int) ([]entity.Candle, error)
-
-	//　データベースを範囲検索
-	// FindRange(ctx context.Context, symbol, interval string, from, to time.Time) ([]entity.Candle, error)
-
-	// 増加分を取得
-	// Latest(ctx context.Context, symbol, interval string) (*entity.Candle, error)
+	Find(ctx context.Context, symbol, interval string, outputsize int) ([]entity.Candle, error)
 
 	// (symbol, interval, time) をユニークキーとしてUpsert
 	UpsertBatch(ctx context.Context, candles []entity.Candle) error
